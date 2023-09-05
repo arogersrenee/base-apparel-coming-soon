d34ba07516d8afc3971d5416aff4ebd7f9837308
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-09-05 12:20:45 -0500

    update script wrapped validation code inside of funtion. Invoke funtion on window load

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-const Email = {
-    verify: function(){
-        const form = document.getElementById("form");
+const formValidation = () => {
+    const emailInput = document.getElementById("email");
+    const submitBtn = document.getElementById("submit-btn");
+    const errorElements = document.querySelectorAll(".no-error");
+    const errorStatment = document.getElementById("error-statement");
+    const form = document.getElementById("form")
 
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            Email.validateEmail();
-        })
-    },
+    const emailRegex =
+    new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
 
-    validateEmail: function (){
-        const emailInput = document.getElementById("email");
-        const submitBtn = document.getElementById("submit-btn");
-        const errorElements = document.querySelectorAll(".no-error");
-        const errorStatment = document.getElementById("error-statement");
-        const emailRegex =
-         new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm"); 
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        validateEmail();
+    })
+
+    const validateEmail = () => {
         
         let message;
         let emailAccess;
 
         if (emailRegex.test(emailInput.value.trim())){
             emailAccess = true;
+            message = "";
             showHideError();
             return true;
         } else if (!emailRegex.test(emailInput.value.trim())){
@@ -29,9 +29,15 @@ const Email = {
             showHint();
             showHideError();
             return false;
+        } else if (emailInput.value.trim() == ""){
+            message = "Please enter your email";
+            emailAccess = false;
+            showHint();
+            showHideError();
+            return false;
         }
 
-        function showHint(){
+        function showHint() {
             errorStatment.innerText = message;
             emailInput.style.border = "2px solid var(--clr-error)";
             emailInput.style.opacity = "1";
@@ -41,99 +47,17 @@ const Email = {
             if(emailAccess){
                 errorElements.forEach(error => {
                     error.classList.add("no-error")
+                    console.log(error)
                 });
             } else {
                 errorElements.forEach(error => {
                     error.classList.remove("no-error")
+                    console.log(error)
                 });
             }
         }
+
     }
-}
+};
 
-window.addEventListener("load", Email.verify);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const emailInput = document.getElementById("email");
-// const submitBtn = document.getElementById("submit-btn");
-// const errorElements = document.querySelectorAll(".no-error");
-// const errorStatment = document.getElementById("error-statement");
-// const form = document.getElementById("form")
-
-// const emailRegex =
-//  new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
-
-// form.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     validateEmail();
-// })
-
-// function validateEmail(){
-    
-//     let message;
-//     let emailAccess;
-
-//     if (emailRegex.test(emailInput.value.trim())){
-//         emailAccess = true;
-//         showHideError()
-//         return true;
-//     } else if (!emailRegex.test(emailInput.value.trim())){
-//         message = "Please enter a valid email";
-//         emailAccess = false;
-//         showHint();
-//         showHideError();
-//         return false;
-//     } else if (emailInput.value.trim() == ""){
-//         message = "Please enter your email";
-//         emailAccess = false;
-//         showHint();
-//         showHideError();
-//         return false;
-//     }
-
-//     function showHint() {
-//         errorStatment.innerText = message;
-//         emailInput.style.border = "2px solid var(--clr-error)";
-//         emailInput.style.opacity = "1";
-//     }
-
-//     function showHideError(){
-//         if(emailAccess){
-//             errorElements.forEach(error => {
-//                 error.classList.add("no-error")
-//                 console.log(error)
-//             });
-//         } else {
-//             errorElements.forEach(error => {
-//                 error.classList.remove("no-error")
-//                 console.log(error)
-//             });
-//         }
-//     }
-
-// }
-
-
-
-
-
+window.addEventListener('load', formValidation);
