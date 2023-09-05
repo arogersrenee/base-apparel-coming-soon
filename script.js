71e36ce128314bb7e9c457b96105036c513d4154
Ashley Rogers <130684731+arogersrenee@users.noreply.github.com>
2023-09-05 10:57:57 -0500

    add script for email validation tryied to wrap function inside of an object. Doesn't work properly I believe because it's only listening for the window onload once.

--- script.js
+++ script.js
@@ -1,60 +1,137 @@
-const emailInput = document.getElementById("email");
-const submitBtn = document.getElementById("submit-btn");
-const errorElements = document.querySelectorAll(".no-error");
-const errorStatment = document.getElementById("error-statement");
-const form = document.getElementById("form")
+const Email = {
+    verify: function(){
+        const form = document.getElementById("form");
 
-const emailRegex =
- new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            Email.validateEmail();
+        })
+    },
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    validateEmail();
-})
+    validateEmail: function (){
+        const emailInput = document.getElementById("email");
+        const submitBtn = document.getElementById("submit-btn");
+        const errorElements = document.querySelectorAll(".no-error");
+        const errorStatment = document.getElementById("error-statement");
+        const emailRegex =
+         new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm"); 
+        
+        let message;
+        let emailAccess;
 
-function validateEmail(){
-    let message;
-    let emailAccess;
+        if (emailRegex.test(emailInput.value.trim())){
+            emailAccess = true;
+            showHideError();
+            return true;
+        } else if (!emailRegex.test(emailInput.value.trim())){
+            message = "Please enter a valid email";
+            emailAccess = false;
+            showHint();
+            showHideError();
+            return false;
+        }
 
-    if (emailRegex.test(emailInput.value.trim())){
-        emailAccess = true;
-        return true;
-    } else if (!emailRegex.test(emailInput.value.trim())){
-        message = "Please enter a valid email";
-        emailAccess = false;
-        showHint();
-        showError();
-        return false;
-    } else if (emailInput.value.trim() == ""){
-        message = "Pleaes enter your email";
-        emailAccess = false;
-        showHint();
-        showError();
-        return false;
-    }
+        function showHint(){
+            errorStatment.innerText = message;
+            emailInput.style.border = "2px solid var(--clr-error)";
+            emailInput.style.opacity = "1";
+        }
 
-    function showHint() {
-        errorStatment.innerText = message;
-        emailInput.style.border = "2px solid var(--clr-error)";
-        emailInput.style.opacity = "1";
-        emailInput.style.color = "var(--clr-error)"
-    }      
-
-    function showError(){
-        if(emailAccess){
-            errorElements.forEach(error => {
-                error.classList.add("no-error")
-                console.log(error)
-            });
-        } else {
-            errorElements.forEach(error => {
-                error.classList.remove("no-error")
-                console.log(error)
-            });
+        function showHideError(){
+            if(emailAccess){
+                errorElements.forEach(error => {
+                    error.classList.add("no-error")
+                });
+            } else {
+                errorElements.forEach(error => {
+                    error.classList.remove("no-error")
+                });
+            }
         }
     }
-
 }
+
+window.addEventListener("load", Email.verify);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const emailInput = document.getElementById("email");
+// const submitBtn = document.getElementById("submit-btn");
+// const errorElements = document.querySelectorAll(".no-error");
+// const errorStatment = document.getElementById("error-statement");
+// const form = document.getElementById("form")
+
+// const emailRegex =
+//  new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/, "gm");
+
+// form.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     validateEmail();
+// })
+
+// function validateEmail(){
+    
+//     let message;
+//     let emailAccess;
+
+//     if (emailRegex.test(emailInput.value.trim())){
+//         emailAccess = true;
+//         showHideError()
+//         return true;
+//     } else if (!emailRegex.test(emailInput.value.trim())){
+//         message = "Please enter a valid email";
+//         emailAccess = false;
+//         showHint();
+//         showHideError();
+//         return false;
+//     } else if (emailInput.value.trim() == ""){
+//         message = "Please enter your email";
+//         emailAccess = false;
+//         showHint();
+//         showHideError();
+//         return false;
+//     }
+
+//     function showHint() {
+//         errorStatment.innerText = message;
+//         emailInput.style.border = "2px solid var(--clr-error)";
+//         emailInput.style.opacity = "1";
+//     }
+
+//     function showHideError(){
+//         if(emailAccess){
+//             errorElements.forEach(error => {
+//                 error.classList.add("no-error")
+//                 console.log(error)
+//             });
+//         } else {
+//             errorElements.forEach(error => {
+//                 error.classList.remove("no-error")
+//                 console.log(error)
+//             });
+//         }
+//     }
+
+// }
 
 
 
